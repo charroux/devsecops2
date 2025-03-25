c60b272d2bc2defc68c5d685bde224246440c5ed
charroux <benoit.charroux@gmail.com>
2025-03-25 14:24:49 +0100

    modif voitures

--- src/main/java/com/example/devsecops2/ServiceWeb.java
+++ src/main/java/com/example/devsecops2/ServiceWeb.java
@@ -18,6 +18,8 @@ public class ServiceWeb {
         cars.add(car);
         car = new Car("CC33DD", "peugeot", 500);
         cars.add(car);
+        car = new Car("DD44EE", "Lancia", 300);
+        cars.add(car);
     }
 
 
